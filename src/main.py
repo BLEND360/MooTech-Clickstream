2dfc335070e129a328addcc63ad2338c6e8856ed
Kathan <kathan.shah@blend360.com>
2023-04-27 15:10:51 -0400

    staging second commit

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-# noinspection PyTypeChecker
 from data_ingestion import DataIngest
 from src.Transformations.month_over_month import MoMTransformer
 from src.Transformations.setup_sliver_layer import SilverLayer
@@ -11,29 +10,32 @@ def update_bronze_layer():
     initializes the DataIngest Class and retrieves the data since last load
     for transaction tables and the latest for SCD tables
     """
-    api_key = dbutils.secrets.get(scope='mootech-scope', key='mootech-key')
+    # api_key = dbutils.secrets.get(scope='mootech-scope', key='mootech-key')
+    api_key = dbutils.secrets.get(scope='my-scope-1', key='api-key')
 
     # update transactions tables
     data_ingest = DataIngest(api_key = api_key)
     start_date = get_latest_transaction_date()
 
-    data_ingest.get_data_by_range(table='clickstream', start_date=start_date)
+    data_ingest.get_data_by_range(table='clickstream', start_date=start_date) # COME UP WITH AN SLA PERIOD
 
-    data_ingest.get_data_by_range(table='transactions', start_date=start_date)
+    # data_ingest.get_data_by_range(table='transactions', start_date=start_date)
     # update SCD tables
-    day_before_yesterday = datetime.datetime.utcnow() - datetime.timedelta(days=2)
-    yesterday = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    # day_before_yesterday = datetime.datetime.utcnow() - datetime.timedelta(days=2)
+    # yesterday = datetime.datetime.utcnow() - datetime.timedelta(days=1)
 
-    data_ingest.get_data_by_range(table='users',
-                                  start_date=day_before_yesterday,
-                                  end_date=yesterday,
-                                  )
+    # data_ingest.get_data_by_range(table='users',
+    #                               start_date=day_before_yesterday,
+    #                               end_date=yesterday,
+    #                               )
 
-    data_ingest.get_data_by_range(table='products',
-                                  start_date=day_before_yesterday,
-                                  end_date=yesterday,
-                                  )
+    # data_ingest.get_data_by_range(table='products',
+    #                               start_date=day_before_yesterday,
+    #                               end_date=yesterday,
+    #                               )
     data_ingest.run_fetch()
+    clickstream_bronze_df = get_clickstream(path=S3Layers.STAGE.value)
+    clickstream_bronze_df.write.mode("overwrite").partitionBy("utc_date").format("delta").save(S3Layers.BRONZE.value+"/clickstream")
     print('bronze layer updated successfully')
 
 def update_silver_layer():
@@ -69,11 +71,11 @@ def main():
     update_bronze_layer()
 
     # update silver layer
-    update_silver_layer()
+    # update_silver_layer()
 
     # generate and save report to gold layer
-    sales_report = generate_report('tumbler')
-    update_gold_layer(sales_report, 'tumbler')
+    # sales_report = generate_report('tumbler')
+    # update_gold_layer(sales_report, 'tumbler')
 
     
 
--- src/utils/S3Layers.py
+++ src/utils/S3Layers.py
@@ -1,12 +1,11 @@
 from enum import Enum
 
-
 class S3Layers(Enum):
     """
     Maps medallion data location to variables
     so that calls are constant
     """
-
+    STAGE = "s3://allstar-training-mootech/stage"
     BRONZE = "s3://allstar-training-mootech/raw_data"
     SILVER = "s3://allstar-training-mootech/silver_layer"
     GOLD = "s3://allstar-training-mootech/results"
